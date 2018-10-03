ff94be2384945d8fe79d198cece7adc9fddb1034
TuGa_Zz_97 <MICAEL.SNNTSR@eduge.ch>
2018-10-03 16:31:58 +0200

    J'ai ajouter les transactions sur le Controlleur addContent

--- addContent.php
+++ addContent.php
@@ -1,40 +1,48 @@
 <?php
-// Script: addContent.php
+// Script: exemple_AddContentAvecTransaction.php
 // Description: Filtre les informations transmise par le formulaire d'upload de fichiers
 // Auteur: Micael Rodrigues
 // Classe : T.IS-E2B
 // Version 1.0: 03.09.2018
+// Version 2.0: 03.10.2018
 
 require_once("library.php");
+require_once("dbconnection.php");
 include("resize-class.php");
 
 $errors = array();
 
 if(filter_has_var(INPUT_POST, "submitContent")){
-    //Check enter comment post user
-    $Comment = filter_input(INPUT_POST, "textArea", FILTER_SANITIZE_STRING);
-
-    $imagePost = $_FILES['inputFile'];
-    $idPost = InsertDataPost($Comment);
-    for($i=0;$i<count($imagePost['name']); $i++)
-    {
-        //Check type,errors,size
-        if (strpos($imagePost["type"][$i], 'image') !== false)
+  //Check enter comment post user
+  $Comment = filter_input(INPUT_POST, "textArea", FILTER_SANITIZE_STRING);
+  $imagePost = $_FILES['inputFile'];
+  if(!empty($Comment)){
+    $db = connectDB();
+    try{
+      $db->beginTransaction();
+      $idPost = InsertDataPost($Comment);
+      //Envoi du message dans la base de donnée
+      //traitement des images
+      if (isset($imagePost)) {
+        for($i=0;$i<count($imagePost['name']); $i++)
         {
+          //Check type,errors,size
+          if (strpos($imagePost["type"][$i], 'image') !== false)
+          {
             echo "is image -> ".$imagePost["type"][$i]."<br>";
             if ($imagePost["error"][$i] == 0)
             {
-                echo "FileName ->".$imagePost["name"][$i]."<br>";
-                echo "as no error -> ".$imagePost["error"][$i]."<br>";
-                if ($imagePost["size"][$i] > 70000000)
-                {
-                    echo "is bigger than 70'000'000 bytes -> ".$imagePost["size"][$i];
-                }
+              echo "FileName ->".$imagePost["name"][$i]."<br>";
+              echo "as no error -> ".$imagePost["error"][$i]."<br>";
+              if ($imagePost["size"][$i] > 70000000)
+              {
+                echo "is bigger than 70'000'000 bytes -> ".$imagePost["size"][$i];
+              }
             }
 
-            /*********************************************************************************************/
-            $name = $imagePost['tmp_name'][$i];  
-            $filename = $imagePost['name'][$i]; 
+            /**************************traitement image*******************************************************************/
+            $name = $imagePost['tmp_name'][$i];
+            $filename = $imagePost['name'][$i];
             //déplace les images dans le répertoire par default
             $uploads_dir = 'img/uploads/';
             //Génère un identifiant unique
@@ -50,34 +58,30 @@ if(filter_has_var(INPUT_POST, "submitContent")){
             // *** 3) Save image
             $resizeObj -> saveImage($uploads_dir.$UUID. "_" .$filename.'', 9);
 
-            /*if(empty($Comment)) //si Commentaire vide --> Upload Image
-            {
-                InsertDataPicture($UUID. "_" .$filename);
-            }*/
-            if(empty($filename)) //Si pas d'image --> juste upload commentaire
-            {
-                $idPost = InsertDataPost($Comment);
+            if(!empty($filename)) //Si pas d'image --> juste upload commentaire
+            /*{
+              $idPost = InsertDataPost($Comment);
             }
-            else{
-                //Ajout dans la BD
-                InsertDataPicturebyId($UUID. "_" .$filename,$idPost);
-                //Redirection vers page principal 
+            else*/{
+              //Ajout dans la BD
+              InsertDataPicturebyId($UUID. "_" .$filename,$idPost);
+              //Redirection vers page principal
             }
-            header('Location: index.php');  
+            header('Location: index.php');
             //Affichage image redimensionnée
             //echo '<br><img src="img/uploads/'.$UUID. "_" .$filename.'">';
+          }
+          $db->commit();
         }
-        else
-        {
-            header('Location: index.php');
-            //Supprime l'image dans le dossier
-            unlink($UUID. "_" .$filename);
-            echo "Erreur d'uploads";
-        }
-
-
-
+      }
+    }catch(Exception $e){
+      echo "Error". $e->getMessage();
+      $db->rollBack();
     }
-
+  }
+  else
+  {
+    header('Location: index.php');
+    echo "Erreur d'uploads";
+  }
 }
-?>
