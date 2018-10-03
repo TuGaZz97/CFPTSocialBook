2b418479b581c28d6151c3c479570f015f8a1343
TuGa_Zz_97 <MICAEL.SNNTSR@eduge.ch>
2018-10-03 16:37:55 +0200

    Modification du controller d'update de posts,  en ajoutant les transactions nécessaires au bon fonctionnnment

--- updatePost.php
+++ updatePost.php
@@ -1,5 +1,32 @@
 <?php
 /* Micael Rodrigues
-* 01.10.2018
-* updatePost.php
+* 24.09.2018
+* deletePost.php
 */
+require_once("library.php");
+require_once("dbconnection.php");
+
+$database = connectDb();
+//Vérifier contenu reçu par le formulaire
+if(filter_has_var(INPUT_POST, "updatePost")){
+
+  try {
+    $database->beginTransaction();
+    
+    $NewComment = filter_input(INPUT_POST, 'CommentModification', FILTER_SANITIZE_STRING);
+    $idPost = filter_input(INPUT_POST, 'idPostModal', FILTER_SANITIZE_STRING);
+
+    if (UpdatePosts($idPost,$NewComment)) {
+      header("Location: index.php");
+      echo "Suppression réussie";
+      $database->commit();
+      header('Location: index.php');
+    } else {
+      throw new Exception('Update_Comment_failed');
+    }
+  } catch(Exception $e) {
+    Debug($e);
+    $database->rollBack();
+    die;
+  }
+}
